6d0a4328fc67569dec3cec0969c1254e8906b141
Belchior Oliveira <belchior@users.noreply.github.com>
2016-10-07 13:22:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-require('./armeiro_modules/coffeescript.js');
 require('./armeiro_modules/browsersync.js');
 require('./armeiro_modules/compressImages.js');
 require('./armeiro_modules/compressSVG.js');
@@ -7,7 +6,6 @@ require('./armeiro_modules/copy.js');
 require('./armeiro_modules/css.js');
 require('./armeiro_modules/js.js');
 require('./armeiro_modules/less.js');
-require('./armeiro_modules/livereload.js');
 require('./armeiro_modules/sass.js');
 require('./armeiro_modules/sprite.js');
 require('./armeiro_modules/doc.js');
