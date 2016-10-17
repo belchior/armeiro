262f5a5ab79ffe6191fa1b4b64402f74992c6245
Belchior Oliveira <belchior@users.noreply.github.com>
2016-10-17 17:11:30 -0200

    remove dependencies to module copy

--- armeiro_modules/copy.js
+++ armeiro_modules/copy.js
@@ -3,76 +3,7 @@ var armeiro = require('./armeirorc.js');
 var gulp = require('gulp');
 
 gulp.task('copy', function () {
-  var cp = require('cp');
-  var glob = require('glob');
-  var gutil = require('gulp-util');
-  var fs = require('fs');
-  var mkdirp = require('mkdirp');
-  var path = require('path');
-
-  if (!Array.isArray(armeiro.copy)) {
-    gutil.log(gutil.colors.red('Armeiro: O atributo copy precisa ser um array'));
-    gutil.log(
-      gutil.colors.red('Armeiro: Consulte a documentação em:'),
-      gutil.colors.underline('https://github.com/belchior/armeiro')
-    );
-    return false;
-  }
-
-  return armeiro.copy.forEach(loadOriginFile);
-
-  function loadOriginFile(item) {
-    glob(item.orig, null, function (err, files) {
-      if (err) {
-        console.error(err);
-        return false;
-      }
-      files.forEach(function (orig) {
-        copyFile(orig, item.dest);
-      });
-    });
-  }
-
-  function copyFile(orig, dest) {
-    fs.stat(dest, function (statError, stat) {
-      if (statError) {
-        if (lookLikeFile(dest)) {
-          mkdirp(path.dirname(dest), function () {
-            cp.sync(orig, dest);
-          });
-        } else if (lookLikeDir(dest)) {
-          mkdirp(dest, function () {
-            cp.sync(orig, path.join(dest, path.basename(orig)));
-          });
-        }
-        return;
-      }
-      if (stat.isDirectory()) {
-        dest += path.basename(orig);
-      }
-      cp.sync(orig, dest);
-    });
-  }
-
-  function lookLikeDir(pathName) {
-    return (
-      typeof pathName === 'string' &&
-      pathName.length > 0 &&
-      pathName[pathName.length-1].match(/[\\/]/)
-    ) || !path.extname(pathName) ? true : false;
-  }
-
-  function lookLikeFile(pathName) {
-    var ext = [
-      '.html', '.css', '.js', '.svg', '.json',
-      '.jpg', '.jpeg', '.png', '.gif',
-      '.markdown', '.md',
-      '.coffee', '.less', '.sass', '.scss'
-    ];
-    return !lookLikeDir() &&
-      path.extname(pathName) &&
-      ext.indexOf(path.extname(pathName)) >= 0
-      ? true : false;
-  }
-
+  return armeiro.copy.forEach(function (item) {
+    gulp.src(item.orig).pipe(gulp.dest(item.dest));
+  });
 });
