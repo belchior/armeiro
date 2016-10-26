eafc17cf1727283d0e2c7d362693197f22f8cbb4
Belchior Oliveira <belchior@outlook.com>
2016-10-25 22:38:31 -0200

    atualização do modulo js

--- armeiro_modules/js.js
+++ armeiro_modules/js.js
@@ -2,79 +2,65 @@
 var armeiro = require('./armeirorc.js');
 var gulp = require('gulp');
 
-gulp.task('build:js', function () {
-  var concat = require('gulp-concat');
-  var sourcemaps = require('gulp-sourcemaps');
-  var uglify = require('gulp-uglify');
-
-  return gulp.src(armeiro.js.orig)
-  .pipe(sourcemaps.init())
-  .pipe(concat(armeiro.js.mainFileCompressed))
-  .pipe(uglify())
-  .pipe(sourcemaps.write('map'))
-  .pipe(gulp.dest(armeiro.js.dest));
-});
 
 gulp.task('compress:js', function () {
   var sourcemaps = require('gulp-sourcemaps');
   var uglify = require('gulp-uglify');
 
-  return gulp.src(armeiro.js.orig)
-  .pipe(sourcemaps.init())
-  .pipe(uglify())
-  .pipe(sourcemaps.write('map'))
-  .pipe(gulp.dest(armeiro.js.dest));
+  armeiro.js.forEach(function (target) {
+    gulp.src(target.src)
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('map'))
+    .pipe(gulp.dest(armeiro.js.dest));
+  });
 });
 
 gulp.task('concat:js', function () {
   var concat = require('gulp-concat');
   var sourcemaps = require('gulp-sourcemaps');
 
-  return gulp.src(armeiro.js.orig)
-  .pipe(sourcemaps.init())
-  .pipe(concat(armeiro.js.mainFile))
-  .pipe(sourcemaps.write('map'))
-  .pipe(gulp.dest(armeiro.js.dest));
-});
-
-gulp.task('delete:js', function () {
-  var deleteFiles = require('./deleteFiles.js');
-
-  deleteFiles({
-    orig: armeiro.js.orig,
-    dest: armeiro.js.dest,
-    extDest: '.js'
-  });
-  deleteFiles({
-    orig: armeiro.js.orig,
-    dest: armeiro.js.dest + 'map/',
-    extDest: '.js.map'
-  });
-  deleteFiles({
-    orig: armeiro.js.dest + armeiro.js.mainFile,
-    dest: armeiro.js.dest,
-    extDest: '.js'
-  });
-  deleteFiles({
-    orig: armeiro.js.dest + armeiro.js.mainFileCompressed,
-    dest: armeiro.js.dest,
-    extDest: '.js'
-  });
-  deleteFiles({
-    orig: armeiro.js.dest + 'map/' + armeiro.js.mainFileCompressed + '.map',
-    dest: armeiro.js.dest + 'map/',
-    extDest: '.map'
+  armeiro.js.forEach(function (target) {
+    gulp.src(target.src)
+    .pipe(sourcemaps.init())
+    .pipe(concat(armeiro.js.name))
+    .pipe(sourcemaps.write('map'))
+    .pipe(gulp.dest(armeiro.js.dest));
   });
 });
 
-gulp.task('watch:js:build', function () {
-  return gulp.watch(armeiro.js.orig, ['build:js']);
+gulp.task('zip:js', function () {
+  var concat = require('gulp-concat');
+  var sourcemaps = require('gulp-sourcemaps');
+  var uglify = require('gulp-uglify');
+
+  armeiro.js.forEach(function (target) {
+    gulp.src(target.src)
+    .pipe(sourcemaps.init())
+    .pipe(concat(armeiro.js.name))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('map'))
+    .pipe(gulp.dest(armeiro.js.dest));
+  });
 });
 
 gulp.task('watch:js:compress', function () {
-  return gulp.watch(armeiro.js.orig, ['compress:js']);
+  armeiro.js.forEach(function (target) {
+    target.watch = target.watch || target.src;
+    gulp.watch(target.watch, ['compress:js']);
+  });
 });
 
 gulp.task('watch:js:concat', function () {
-  return gulp.watch(armeiro.js.orig, ['concat:js']);
+  armeiro.js.forEach(function (target) {
+    target.watch = target.watch || target.src;
+    gulp.watch(target.watch, ['concat:js']);
+  });
+});
+
+gulp.task('watch:js:zip', function () {
+  armeiro.js.forEach(function (target) {
+    target.watch = target.watch || target.src;
+    gulp.watch(target.watch, ['zip:js']);
+  });
 });
