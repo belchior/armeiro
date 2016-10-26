87d59be587e909b9e95d80d1331cb9995d15a235
Belchior Oliveira <belchior@outlook.com>
2016-10-25 22:37:32 -0200

    atualização do modulo less

--- armeiro_modules/less.js
+++ armeiro_modules/less.js
@@ -3,34 +3,21 @@ var armeiro = require('./armeirorc.js');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var errorHandler = function (err) {
-  gutil.log(gutil.colors.red('ERROR', 'compile:less'), err);
-  this.emit('end', new gutil.PluginError('compile:less', err, {showStack: false}));
-}
-
-gulp.task('build:less', function () {
-  var concat = require('gulp-concat');
-  var less = require('gulp-less');
-  var cssnano = require('gulp-cssnano');
-  var sourcemaps = require('gulp-sourcemaps');
-
-  return gulp.src(armeiro.less.orig)
-  .pipe(sourcemaps.init())
-  .pipe(less().on('error', errorHandler))
-  .pipe(concat(armeiro.less.mainFileCompressed))
-  .pipe(cssnano())
-  .pipe(sourcemaps.write('map'))
-  .pipe(gulp.dest(armeiro.less.dest));
-});
+  gutil.log(gutil.colors.red('ERROR', 'Armeiro:less'), err);
+  this.emit('end', new gutil.PluginError('Armeiro:less', err, {showStack: false}));
+};
 
 gulp.task('compile:less', function () {
   var less = require('gulp-less');
   var sourcemaps = require('gulp-sourcemaps');
 
-  return gulp.src(armeiro.less.orig)
-  .pipe(sourcemaps.init())
-  .pipe(less().on('error', errorHandler))
-  .pipe(sourcemaps.write('map'))
-  .pipe(gulp.dest(armeiro.less.dest));
+  armeiro.less.forEach(function (target) {
+    gulp.src(target.src)
+    .pipe(sourcemaps.init())
+    .pipe(less().on('error', errorHandler))
+    .pipe(sourcemaps.write('map'))
+    .pipe(gulp.dest(target.dest));
+  });
 });
 
 gulp.task('compress:less', function () {
@@ -38,12 +25,14 @@ gulp.task('compress:less', function () {
   var cssnano = require('gulp-cssnano');
   var sourcemaps = require('gulp-sourcemaps');
 
-  return gulp.src(armeiro.less.orig)
-  .pipe(sourcemaps.init())
-  .pipe(less().on('error', errorHandler))
-  .pipe(cssnano())
-  .pipe(sourcemaps.write('map'))
-  .pipe(gulp.dest(armeiro.less.dest));
+  armeiro.less.forEach(function (target) {
+    gulp.src(target.src)
+    .pipe(sourcemaps.init())
+    .pipe(less().on('error', errorHandler))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('map'))
+    .pipe(gulp.dest(target.dest));
+  });
 });
 
 gulp.task('concat:less', function () {
@@ -51,56 +40,58 @@ gulp.task('concat:less', function () {
   var less = require('gulp-less');
   var sourcemaps = require('gulp-sourcemaps');
 
-  return gulp.src(armeiro.less.orig)
-  .pipe(sourcemaps.init())
-  .pipe(less().on('error', errorHandler))
-  .pipe(concat(armeiro.less.mainFile))
-  .pipe(sourcemaps.write('map'))
-  .pipe(gulp.dest(armeiro.less.dest));
-});
-
-gulp.task('delete:less', function () {
-  var deleteFiles = require('./deleteFiles.js');
-
-  deleteFiles({
-    orig: armeiro.less.orig,
-    dest: armeiro.less.dest,
-    extDest: '.css'
-  });
-  deleteFiles({
-    orig: armeiro.less.orig,
-    dest: armeiro.less.dest + 'map/',
-    extDest: '.css.map'
-  });
-  deleteFiles({
-    orig: armeiro.less.dest + armeiro.less.mainFile,
-    dest: armeiro.less.dest,
-    extDest: '.css'
-  });
-  deleteFiles({
-    orig: armeiro.less.dest + armeiro.less.mainFileCompressed,
-    dest: armeiro.less.dest,
-    extDest: '.css'
-  });
-  deleteFiles({
-    orig: armeiro.less.dest + 'map/' + armeiro.less.mainFileCompressed + '.map',
-    dest: armeiro.less.dest + 'map/',
-    extDest: '.map'
+  armeiro.less.forEach(function (target) {
+    gulp.src(target.src)
+    .pipe(sourcemaps.init())
+    .pipe(concat(target.name))
+    .pipe(less().on('error', errorHandler))
+    .pipe(sourcemaps.write('map'))
+    .pipe(gulp.dest(target.dest));
   });
 });
 
-gulp.task('watch:less:build', function () {
-  return gulp.watch(armeiro.less.orig, ['build:less']);
+gulp.task('zip:less', function () {
+  var concat = require('gulp-concat');
+  var cssnano = require('gulp-cssnano');
+  var less = require('gulp-less');
+  var sourcemaps = require('gulp-sourcemaps');
+
+  armeiro.less.forEach(function (target) {
+    gulp.src(target.src)
+    .pipe(sourcemaps.init())
+    .pipe(concat(target.name))
+    .pipe(less().on('error', errorHandler))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('map'))
+    .pipe(gulp.dest(target.dest));
+  });
 });
+
 
 gulp.task('watch:less:compile', function () {
-  return gulp.watch(armeiro.less.orig, ['compile:less']);
+  armeiro.less.forEach(function (target) {
+    target.watch = target.watch || target.src;
+    gulp.watch(target.watch, ['compile:less']);
+  });
 });
 
 gulp.task('watch:less:compress', function () {
-  return gulp.watch(armeiro.less.orig, ['compress:less']);
+  armeiro.less.forEach(function (target) {
+    target.watch = target.watch || target.src;
+    gulp.watch(target.watch, ['compress:less']);
+  });
 });
 
 gulp.task('watch:less:concat', function () {
-  return gulp.watch(armeiro.less.orig, ['concat:less']);
+  armeiro.less.forEach(function (target) {
+    target.watch = target.watch || target.src;
+    gulp.watch(target.watch, ['concat:less']);
+  });
+});
+
+gulp.task('watch:less:zip', function () {
+  armeiro.less.forEach(function (target) {
+    target.watch = target.watch || target.src;
+    gulp.watch(target.watch, ['zip:less']);
+  });
 });
