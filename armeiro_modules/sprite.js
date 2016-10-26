e4ad206b3e1a8ab9deb388de5b9bddc7c15ffd7a
Belchior Oliveira <belchior@outlook.com>
2016-10-25 22:39:21 -0200

    atualização do modulo sprite

--- armeiro_modules/sprite.js
+++ armeiro_modules/sprite.js
@@ -2,24 +2,18 @@
 var armeiro = require('./armeirorc.js');
 var gulp = require('gulp');
 
-gulp.task('delete:sprite', function () {
-  var del = require('del');
 
-  del([
-    armeiro.sprite.dest + armeiro.sprite.name,
-    armeiro.sprite.dest + armeiro.sprite.cssName
-  ]);
-});
-
-gulp.task('build:sprite', function () {
+gulp.task('sprite', function () {
   var spritesmith = require('gulp.spritesmith');
 
-  var spriteData = gulp.src(armeiro.sprite.orig)
-  .pipe(spritesmith({
-    algorithm: 'top-down',
-    imgName: armeiro.sprite.imgName,
-    imgPath: armeiro.sprite.imgPath,
-    cssName: armeiro.sprite.cssName
-  }));
-  return spriteData.pipe(gulp.dest(armeiro.sprite.dest));
+  armeiro.sprite.forEach(function (target) {
+    gulp.src(target.src)
+    .pipe(spritesmith({
+      algorithm: target.algorithm,
+      imgName: target.imgName,
+      imgPath: target.imgPath,
+      cssName: target.cssName
+    }))
+    .pipe(gulp.dest(target.dest));
+  });
 });
