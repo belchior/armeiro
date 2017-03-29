31ace27444b4cd765802850a37b95fbeb314e43c
Belchior Oliveira <belchior@users.noreply.github.com>
2017-03-28 22:21:48 -0300

    inclusão de gulp-rename como dependênia do módulo copy

--- armeiro_modules/copy.js
+++ armeiro_modules/copy.js
@@ -4,10 +4,17 @@ var gulp = require('gulp');
 
 gulp.task('copy', function () {
   var merged = require(armeiro.pathModules + 'merge-stream.js')();
+  var rename = require('gulp-rename');
 
   armeiro.copy.forEach(function (target) {
     merged.add(
       gulp.src(target.src, target.options)
+      .pipe(rename(function (path) {
+        target.name ? path.basename = target.name : '';
+        target.prefix ? path.basename = target.prefix + path.basename : '';
+        target.suffix ? path.basename = path.basename + target.suffix : '';
+        target.extname ? path.extname = target.extname : '';
+      }))
       .pipe(gulp.dest(target.dest))
     );
   });
